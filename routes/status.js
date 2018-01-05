6a9f1167f3d152c0ae503f7d4c0acb52515e48bd
gverni <g.verni@gmail.com>
2018-01-05 10:15:52 +0000

    Clean-up of comments

--- routes/status.js
+++ routes/status.js
@@ -1,26 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
-// var ping = require('ping')
-//
-// var pingPromise = (host) => {
-//   return new Promise((resolve, reject) => {
-//     ping.sys.probe(host.address, function (isAlive) {
-//       resolve(host.name + ': ' + (isAlive ? 'Alive' : 'Not reachable'))
-//     })
-//   })
-// }
-
 router.get('/', function (req, res, next) {
   var config = req.app.get('config')
-  // var promises = []
-  // config.hosts.forEach(function (host) {
-  //   promises.push(pingPromise(host))
-  // })
-  //
-  // Promise.all(promises).then((results) => {
-  //   res.send(results.join('<p>'))
-  // })
   res.render('status', { hosts: config.hosts })
 })
 
