cb7bcbb11d0911099dd6ddfff0b2d7a9ca655761
gverni <g.verni@gmail.com>
2018-01-07 15:17:05 +0000

    Re-implemented input to REST API

--- routes/service.js
+++ routes/service.js
@@ -1,9 +1,29 @@
 
+/* REST API input parameters
+ host: hosts[i].name in config.json
+ service: host[i].service[i].name in config.json
+*/
+
 var express = require('express')
 var ping = require('ping')
 const request = require('request')
 
 var router = express.Router()
+
+/* Function that returns the first object whose <idKey> === <idValue>
+  arrayOfObject: array of Objects
+  idKey: object key that holds the id
+  idValue: value of the id
+*/
+function findObject (arrayOfObjects, idKey, idValue) {
+  let returnObject
+  arrayOfObjects.forEach(function (object) {
+    if (object.hasOwnProperty(idKey)) {
+      if (object[idKey] === idValue) { returnObject = object }
+    }
+  })
+  return returnObject
+}
 
 var pingPromise = (host) => {
   return new Promise((resolve, reject) => {
@@ -15,25 +35,42 @@ var pingPromise = (host) => {
 }
 
 router.get('/', function (req, res, next) {
-  if (req.query.protocol === 'ping') {
-    pingPromise(req.query.address).then((isAlive) => {
-      res.send({status: 'success', isAlive: isAlive})
-    }).catch((reason) => {
-      res.send({status: 'error', details: 'Error on ping: ' + reason})
-    })
-  } else if (req.query.protocol.startsWith('http')) {
-    var options = {
-      url: req.query.protocol + '://' + req.query.address + ':' + req.query.port + '/' + req.query.path,
-      timeout: 5000 }
-    request(options, function (error, response, body) {
-      if (error) {
-        res.send({status: 'error', details: error.code})
+  var config = req.app.get('config')
+  if (req.query.host && req.query.service) {
+    let host = findObject(config.hosts, 'name', req.query.host)
+    if (host) {
+      let service = findObject(host.services, 'name', req.query.service)
+      if (service) {
+        if (service.protocol === 'ping') {
+          console.log('Pinging ' + host.address)
+          pingPromise(host.address).then((isAlive) => {
+            res.send({status: 'success', isAlive: isAlive})
+          }).catch((reason) => {
+            res.send({status: 'error', details: 'Error on ping: ' + reason})
+          })
+        } else if (service.protocol.startsWith('http')) {
+          console.log('HHTP(s)ing ' + host.address)
+          var options = {
+            url: service.protocol + '://' + host.address + ':' + service.port + '/' + service.path,
+            timeout: 5000 }
+          request(options, function (error, response, body) {
+            if (error) {
+              res.send({status: 'error', details: error})
+            } else {
+              res.send({status: 'success', statusCode: response.statusCode})
+            }
+          })
+        } else {
+          res.send({status: 'error', details: 'No protocol specified'})
+        }
       } else {
-        res.send({status: 'success', statusCode: response.statusCode})
+        res.send({status: 'error', details: 'service name not found'})
       }
-    })
+    } else {
+      res.send({status: 'error', details: 'host name not found'})
+    }
   } else {
-    res.send({status: 'error', details: 'No protocol specified'})
+    res.send({status: 'error'})
   }
 })
 
